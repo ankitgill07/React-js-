2ab1f107067cb7b7272470be680a4917943ab0ab
ankitgill <gillankit076@gmail.com>
2024-11-14 16:22:17 +0530

    Message

---
+++ youtubeApp/src/api.js
@@ -0,0 +1,3 @@
+export const  YOUTUBE_APP_URL="https://www.googleapis.com/youtube/v3/"
+export const  YOUTUBE_API_KEY= `${"AIzaSyCW6sZ0RB6mPVVhcmYoz0N7PC1z8bZBwww" && "AIzaSyDNP5V2VWwmgdCo37esOMswHerHO08EzDI" && "AIzaSyCVm7hTOksDxa7ETPdovonJa7kQ9NNON40" }`
+export const SEARCH_SUGGESTIONS_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
